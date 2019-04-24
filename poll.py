b5b301cb69dffa4f6c06ef6b15e80db06e3f6c1a
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-04-24 14:50:09 +0200

    add comments and formatting

--- poll.py
+++ poll.py
@@ -4,20 +4,16 @@ import datetime
 from time import sleep
 from pigpio_DHT22 import DHT22
 
-# this connects to the pigpio daemon which must be started first
+# this connects to the pigpio daemon - needs to be started in CLI first with "sudo pigpiod"
 pi = pigpio.pi()
 
-s = DHT22.sensor(pi, 4)
-time = datetime.datetime.now()
-
-
+s = DHT22.sensor(pi, 4) # initialize sensor on the pi, gpio#4
+time = datetime.datetime.now() # shortening this call
 
 def poll():
     s.trigger()
-    sleep(.05) # Necessary on faster Raspberry Pi's
+    sleep(.05) # sleep in seconds - needed to allow registering of data to complete before reading
     write(s.humidity(), s.temperature())
-    print('Humidity:' + '{:3.0f}'.format(s.humidity() / 1.) + '%')
-    print('Temperature:' + '{:3.0f}'.format(s.temperature() / 1.) + 'c')
 
 def write(humidity, temperature):
     now = time.strftime("%Y-%m-%d %H:%M:%S")
@@ -27,8 +23,9 @@ with open('log.csv', 'w') as file:
     writer = csv.writer(file, lineterminator='\n')
     writer.writerow(['time', 'humidity', 'temperature'])
     poll()
-    sleep(5)
+    sleep(5) # sleep in seconds - DHT22 is limited to 1 poll per 2seconds
     poll()
-    
+
+# cleaning up
 s.cancel()
 pi.stop()
