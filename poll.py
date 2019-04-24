7d373053b867f272e7416475df19243901347409
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-04-24 14:45:13 +0200

    writes sensor data to csv file using pythons csv and datetime module

--- poll.py
+++ poll.py
@@ -1,4 +1,6 @@
 import pigpio
+import csv
+import datetime
 from time import sleep
 from pigpio_DHT22 import DHT22
 
@@ -6,16 +8,27 @@ from pigpio_DHT22 import DHT22
 pi = pigpio.pi()
 
 s = DHT22.sensor(pi, 4)
+time = datetime.datetime.now()
+
+
 
 def poll():
     s.trigger()
     sleep(.05) # Necessary on faster Raspberry Pi's
+    write(s.humidity(), s.temperature())
     print('Humidity:' + '{:3.0f}'.format(s.humidity() / 1.) + '%')
     print('Temperature:' + '{:3.0f}'.format(s.temperature() / 1.) + 'c')
 
-while True:
+def write(humidity, temperature):
+    now = time.strftime("%Y-%m-%d %H:%M:%S")
+    writer.writerow([now, str(humidity), int(temperature)])
+
+with open('log.csv', 'w') as file:
+    writer = csv.writer(file, lineterminator='\n')
+    writer.writerow(['time', 'humidity', 'temperature'])
     poll()
-    sleep(3)
+    sleep(5)
+    poll()
     
 s.cancel()
 pi.stop()
