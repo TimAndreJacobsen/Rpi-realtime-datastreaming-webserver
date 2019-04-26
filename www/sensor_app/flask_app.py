a5dd45d4beffe4e30e7cc9dc88712eabdbe77e9d
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-04-26 16:47:03 +0200

    playing around with flask

--- www/sensor_app/flask_app.py
+++ www/sensor_app/flask_app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World! Flask here"
 
+@app.route("/data")
+def show_data():
+    return "<h1>Headline 1</h1><br><p>lots of cool shiny data here, come get it while its fresh!</p>"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
