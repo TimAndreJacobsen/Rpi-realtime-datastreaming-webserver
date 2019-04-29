2a9fa6e7aa6abe6ec8cad946eae6187501e150bc
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-04-29 16:17:18 +0200

    import Adafruit_DHT lib and send sensor data to template

--- www/sensor_app/flask_app.py
+++ www/sensor_app/flask_app.py
@@ -1,17 +1,21 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
+import sys
+import Adafruit_DHT
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("hello.html", message="hello world, flask with render here!")
 
 @app.route("/data")
 def show_data():
-    return "<h1>Headline 1</h1><br><p>lots of cool shiny data here, come get it while its fresh!</p>"
-
+    humidity, temp = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+    if humidity is not None and temp is not None:
+        return render_template("room_data.html", temp=temp, humidity=humidity)
+    else:
+        return render_template("sensor_error.html", msg="temp/humid is None")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
