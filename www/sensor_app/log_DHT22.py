ba6f6dc81bfa6dd2dd8288d47ed97039c949e6ad
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-02 12:18:26 +0200

    basic sensor read & log function complete - could be improved according to SRP

--- www/sensor_app/log_DHT22.py
+++ www/sensor_app/log_DHT22.py
@@ -9,3 +9,16 @@ def log_values(sensor_id, temperature, humidity):
     connection = sqlite3.connect('/var/www/sensor_app/sensor_app.db')
     cursor = connection.cursor()
 
+    cursor.execute("""INSERT INTO temperatures values(datetime(CURRENT_TIMESTAMP, 'localtime'), (?), (?))""")
+    cursor.execute("""INSERT INTO humidities values(datetime(CURRENT_TIMESTAMP, 'localtime'), (?), (?))""")
+    
+    connection.commit()
+    connection.close()
+
+humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+if humidity is not None and temperature is not None:
+    log_values("ceiling", temperature, humidity)
+    # set up out of optimal range notification
+else:
+    log_values("ceiling", -998, -998)
+    # set up error reporting
