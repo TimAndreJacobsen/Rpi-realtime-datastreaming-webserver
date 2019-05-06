bb1ca24cdea8a023e4a1ca689caf6441c6153281
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-06 12:42:17 +0200

    mark jQuery event handlers as passive

---
+++ www/sensor_app/static/js/jQuery_setPassive.js
@@ -0,0 +1,42 @@
+jQuery.event.special.touchstart = 
+{
+  setup: function( _, ns, handle )
+  {
+    if ( ns.includes("noPreventDefault") ) 
+    {
+      this.addEventListener("touchstart", handle, { passive: false });
+    } 
+    else 
+    {
+      this.addEventListener("touchstart", handle, { passive: true });
+    }
+  }
+};
+jQuery.event.special.touchmove = 
+{
+  setup: function( _, ns, handle )
+  {
+    if ( ns.includes("noPreventDefault") ) 
+    {
+      this.addEventListener("touchstart", handle, { passive: false });
+    } 
+    else 
+    {
+      this.addEventListener("touchstart", handle, { passive: true });
+    }
+  }
+};
+jQuery.event.special.mousewheel = 
+{
+  setup: function( _, ns, handle )
+  {
+    if ( ns.includes("noPreventDefault") ) 
+    {
+      this.addEventListener("touchstart", handle, { passive: false });
+    } 
+    else 
+    {
+      this.addEventListener("touchstart", handle, { passive: true });
+    }
+  }
+};
