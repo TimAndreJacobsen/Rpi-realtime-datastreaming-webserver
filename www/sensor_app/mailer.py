5da5a3105217c2e88e6f81702ef9211113c2eb5b
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-19 15:43:46 +0200

    login to google gmail working

--- www/sensor_app/mailer.py
+++ www/sensor_app/mailer.py
@@ -2,6 +2,10 @@ import smtplib
 import os
 
 mail_password = os.environ.get('RPI_MAIL_GOOGLE_PASS')
+mail_user = 'tymmyh@gmail.com'
 
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(user=mail_user, password=mail_password)
