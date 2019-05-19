cfc5905ffff66902ccdcd9f0807b79ac2727e210
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-19 15:44:10 +0200

    sending emails work, using gmail

--- www/sensor_app/mailer.py
+++ www/sensor_app/mailer.py
@@ -9,3 +9,10 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.starttls()
     smtp.ehlo()
     smtp.login(user=mail_user, password=mail_password)
+
+    subject = 'subject goes here'
+    body = 'body message here'
+
+    msg = f'Subject: {subject}\n\n{body}'
+
+    smtp.sendmail(mail_user, 'to_email_here', msg)
