ee3c1b68caee1ab5e19b05b12a9d862a644e915e
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-24 11:31:39 +0200

    hid address

--- www/sensor_app/mailer.py
+++ www/sensor_app/mailer.py
@@ -2,7 +2,8 @@ import smtplib
 import os
 
 mail_password = os.environ.get('RPI_MAIL_GOOGLE_PASS')
-mail_user = 'tymmyh@gmail.com'
+mail_user = os.environ.get('MY_MAIL_ADR')
+mail_receiver = os.environ.get('MAIL_ADR')
 
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.ehlo()
