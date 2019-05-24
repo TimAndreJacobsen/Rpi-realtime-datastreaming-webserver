b9aa48af0b500342a480109bccc4a589fdfbd8ab
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-24 12:05:22 +0200

    added timer to prevent function firing too rapidly and spamming mails
    currently set at 1 hour

--- www/sensor_app/log_DHT22.py
+++ www/sensor_app/log_DHT22.py
@@ -2,22 +2,27 @@
 # Records from DHT_22 sensor and logs into SQLite3 database
 
 import sqlite3
-import Adafruit_DHT
+import time
 import gevent
+import Adafruit_DHT
 import mailer
+
+
+last_time_called = None # initializing 
 
 def log_values(sensor_id, temperature, humidity):
     connection = sqlite3.connect('/var/www/sensor_app/sensor_app.db')
     cursor = connection.cursor()
-
     cursor.execute("""INSERT INTO temperatures values(datetime(CURRENT_TIMESTAMP, 'localtime'), (?), (?))""", (sensor_id, temperature))
     cursor.execute("""INSERT INTO humidities values(datetime(CURRENT_TIMESTAMP, 'localtime'), (?), (?))""", (sensor_id, humidity))
     connection.commit()
     connection.close()
 
-def send_alert():
-    mailer.send(temperature, humidity)
 
+def send_alert():
+    if last_time_called is None or last_time_called + 3600 < time.time(): 
+        mailer.send(temperature, humidity)
+        last_time_called = time.time()
 
 humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
 if humidity is not None and temperature is not None:
@@ -26,7 +31,7 @@ if humidity is not None and temperature is not None:
             if temperature > 27 or temperature < 10:
                 send_alert()
             log_values("ceiling", temperature, humidity)
-            # set up out of optimal range notification
+
 else:
     while humidity is None:
         humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
