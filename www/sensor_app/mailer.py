890467842670970f0b5c52e50be5342aa5511ca3
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-24 11:34:42 +0200

    wrapped send mail in a function

--- www/sensor_app/mailer.py
+++ www/sensor_app/mailer.py
@@ -5,15 +5,16 @@ mail_password = os.environ.get('RPI_MAIL_GOOGLE_PASS')
 mail_user = os.environ.get('MY_MAIL_ADR')
 mail_receiver = os.environ.get('MAIL_ADR')
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
-    smtp.login(user=mail_user, password=mail_password)
+def send(temp, humid):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(user=mail_user, password=mail_password)
 
-    subject = 'subject goes here'
-    body = 'body message here'
+        subject = 'subject goes here'
+        body = 'temperature is {}\nhumidity is {}'.format(temp, humid)
 
-    msg = f'Subject: {subject}\n\n{body}'
+        msg = f'Subject: {subject}\n\n{body}'
 
-    smtp.sendmail(mail_user, 'to_email_here', msg)
+        smtp.sendmail(mail_user, mail_user, msg)
