b5428972fd83a65b83fb74896ede59201aea12c0
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-26 16:28:20 +0200

    add html stub

--- www/sensor_app/mailer.py
+++ www/sensor_app/mailer.py
@@ -14,6 +14,24 @@ def send(temp, humid):
 
         subject = 'subject goes here'
         body = 'temperature is {}\nhumidity is {}'.format(temp, humid)
+        html = """\
+                <html>
+                        <head></head>
+                        <body>
+
+                        </body>
+                </html>
+                """
+
+        # Record the MIME types of both parts - text/plain and text/html.
+        part1 = MIMEText(text, 'plain')
+        part2 = MIMEText(html, 'html')
+
+        # Attach parts into message container.
+        # According to RFC 2046, the last part of a multipart message, in this case
+        # the HTML message, is best and preferred.
+        msg.attach(part1)
+        msg.attach(part2)
 
         msg = f'Subject: {subject}\n\n{body}'
 
