323c2b455f747245a6dd314225293f749c9ed84f
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-27 10:27:41 +0200

    updated mailer to follow 3.7 documentation
    https://docs.python.org/3/library/email.examples.html

--- www/sensor_app/mailer.py
+++ www/sensor_app/mailer.py
@@ -1,38 +1,28 @@
 import smtplib
 import os
+from email.message import EmailMessage
+from email.headerregistry import Address
 
 mail_password = os.environ.get('RPI_MAIL_GOOGLE_PASS')
 mail_user = os.environ.get('MY_MAIL_ADR')
 mail_receiver = os.environ.get('MAIL_ADR')
 
 def send(temp, humid):
+    msg = EmailMessage()
+    msg['Subject'] = "Ayons asperges pour le déjeuner"
+    msg['From'] = Address("RPI-server", "humitemp", mail_user)
+    msg['To'] = (mail_receiver)
+    msg.set_content("""\
+    This is an automated alert service.
+    the current temperature is out of optimal range.
+    temperature is {}, optimally you want to stay under 26c
+    humidity is {}, higher temps benefit from higher humidity
+    """.format(temp, humid))
+
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.ehlo()
         smtp.starttls()
         smtp.ehlo()
         smtp.login(user=mail_user, password=mail_password)
+        smtp.send_message(msg)
 
-        subject = 'subject goes here'
-        body = 'temperature is {}\nhumidity is {}'.format(temp, humid)
-        html = """\
-                <html>
-                        <head></head>
-                        <body>
-
-                        </body>
-                </html>
-                """
-
-        # Record the MIME types of both parts - text/plain and text/html.
-        part1 = MIMEText(text, 'plain')
-        part2 = MIMEText(html, 'html')
-
-        # Attach parts into message container.
-        # According to RFC 2046, the last part of a multipart message, in this case
-        # the HTML message, is best and preferred.
-        msg.attach(part1)
-        msg.attach(part2)
-
-        msg = f'Subject: {subject}\n\n{body}'
-
-        smtp.sendmail(mail_user, mail_user, msg)
