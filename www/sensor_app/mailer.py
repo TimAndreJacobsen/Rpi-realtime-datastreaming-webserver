dc87b9afa4e294627ddb5c1a1ec6d86bb70749e2
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-27 12:07:26 +0200

    format floating point precision down to 1 decimal

--- www/sensor_app/mailer.py
+++ www/sensor_app/mailer.py
@@ -7,7 +7,7 @@ mail_password = os.environ.get('RPI_MAIL_GOOGLE_PASS')
 mail_user = os.environ.get('MY_MAIL_ADR')
 mail_receiver = os.environ.get('MAIL_ADR')
 
-def send(temp, humid):
+def send(temp, humid, last_time):
     msg = EmailMessage()
     msg['Subject'] = "Automated temperature alert"
     msg['From'] = Address("RPI-server", "humitemp", mail_user)
@@ -16,13 +16,14 @@ def send(temp, humid):
     <html>
       <head></head>
       <body>
-        <p>This is an automated alert service.</p>
+        <h1>This is an automated alert service.</h1>
         <p>the current temperature is out of optimal range.</p>
-        <p>temperature is {}, optimally you want to stay under 26c<p/>
-        <p>humidity is {}, higher temps benefit from higher humidity<p/>
+        <p>temperature is {:0.1f}, optimally you want to stay under 26c<p/>
+        <p>humidity is {:0.1f}, higher temps benefit from higher humidity<p/>
+        <p>last alert sent at {:0.0f}</p>
       </body>
     </html>
-    """.format(temp, humid), subtype='html')
+    """.format(temp, humid, last_time), subtype='html')
 
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.ehlo()
@@ -30,4 +31,3 @@ def send(temp, humid):
         smtp.ehlo()
         smtp.login(user=mail_user, password=mail_password)
         smtp.send_message(msg)
-
