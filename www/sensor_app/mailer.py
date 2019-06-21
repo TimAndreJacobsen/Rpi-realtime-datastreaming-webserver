1e6107d23b270b3437427c245516956b7690ff25
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-06-21 12:48:56 +0200

    changed mail body, so it's easier to get important info from notification body

--- www/sensor_app/mailer.py
+++ www/sensor_app/mailer.py
@@ -9,6 +9,7 @@ mail_password = os.environ.get('RPI_MAIL_GOOGLE_PASS')
 mail_user = os.environ.get('MY_MAIL_ADR')
 mail_receiver = os.environ.get('MAIL_ADR')
 
+
 def send(temp, humid, last_time):
     msg = EmailMessage()
     msg['Subject'] = "Automated temperature alert"
@@ -18,20 +19,15 @@ def send(temp, humid, last_time):
     <html>
       <head></head>
       <body>
-        <h1>This is an automated alert service.</h1>
-        <p>the current temperature is out of optimal range.</p>
         <p>temperature is {:0.1f}, optimally you want to stay under 26c<p/>
         <p>humidity is {:0.1f}, higher temps benefit from higher humidity<p/>
-        <p>last alert sent at {:0.0f}</p>
-        <p>current time {:0.0f}</p>
-        <p>elapsed time since last alert {:0.0f} minutes</p>
       </body>
     </html>
     """.format(temp, humid,
                config.CNF['last_time_called'],
                time.time(),
                (time.time() - config.CNF['last_time_called'])),
-               subtype='html')
+        subtype='html')
 
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.ehlo()
